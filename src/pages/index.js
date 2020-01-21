bf647165d115327036765fc7b6ad40fd47387f94
ankit-patel10 <ankitp5469@gmail.com>
2020-01-21 02:02:13 -0500

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,10 +14,10 @@ class IndexPage extends React.Component {
       	<div>
     <h1>Ankit Patel</h1>
     </div>
-    <p>Hello, I am a first year <span className='red'> Software Engineering student </span>  at the
+    <p>Hello, I am a third year <span className='red'> Software Engineering student </span>  at the
        University of Waterloo. My favourite things to do (besides programming) are playing video games, 
        reading Science Fiction novels, playing the piano, and socializing with friends. I am interested in occupying a
-       <span className='green'> full-stack web development </span> or a<span className='green'> full-stack mobile development </span>position.</p>
+       <span className='green'> full-stack web development </span> position.</p>
     <p></p>
     <a href='/ResumeCurrent.pdf' target='_blank'><button className='button-purple'> RÉSUMÉ </button></a>
     <p></p>
